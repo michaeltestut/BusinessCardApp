28232c81fc169b26474d5d347f31255436b947ad
Michael Testut <michael.testut@yahoo.com>
2023-01-26 20:20:30 -0800

    Initial Commit

---
+++ BusinessCard/BusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCardApp.swift
+//  BusinessCard
+//
+//  Created by Michael Testut on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct BusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
